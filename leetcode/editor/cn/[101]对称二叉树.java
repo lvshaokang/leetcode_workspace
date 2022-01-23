dd618e979503b6ec6137a70da3f7c513dbf834e3
Red <lvshaokang@hotmail.com>
2022-01-23 16:45:20 +0800

    feat(101): 对称二叉树

---
+++ leetcode/editor/cn/[101]对称二叉树.java
@@ -0,0 +1,70 @@
+//给你一个二叉树的根节点 root ， 检查它是否轴对称。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,2,2,3,4,4,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,2,2,null,3,null,3]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [1, 1000] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：你可以运用递归和迭代两种方法解决这个问题吗？ 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1714 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * 判断一个二叉树是镜像对称的,等价于,判断左右子树是镜像对称的
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p != null && q != null && p.val == q.val) { // 非空 & 节点值相等
+            // p.left = q.right && p.right = q.left
+            return isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);
+        }
+        return false;
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
