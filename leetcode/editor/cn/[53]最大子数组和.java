12f60866f01d8a40c32b5a4639002765f58db9e9
Red <lvshaokang@hotmail.com>
2022-03-12 19:22:16 +0800

    chore(53): 最大子数组和

--- leetcode/editor/cn/[53]最大子数组和.java
+++ leetcode/editor/cn/[53]最大子数组和.java
@@ -48,7 +48,7 @@ class Solution {
         // dp[i] = dp[i-1] <= 0 ? nums[i] : dp[i-1] + nums[i]
         int[] dp = new int[n];
         dp[0] = nums[0];
-        int res = dp[0]; // 全局最大和,nums[i] < 0时存在付代价
+        int res = dp[0]; // 全局最大和,nums[i] < 0,负代价
         for (int i = 1; i < n; i++) {
             if (dp[i-1] <= 0) {
                 dp[i] = nums[i];
